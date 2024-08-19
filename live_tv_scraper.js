8d26f1e87b90fb8f467f44319826bd5dc9c3385a
Pedro <pedroalabart@hotmail.com>
2024-08-19 20:50:04 -0300

    Live TV scrapea bien

--- live_tv_scraper.js
+++ live_tv_scraper.js
@@ -1,59 +1,55 @@
 import puppeteer from 'puppeteer'
+import fs from 'fs'
+
+const timer = "Live TV - Scraping Time"
 
 const scraper_starter = async () => {
-  console.time()
+  console.time(timer)
 
-    const browser = await puppeteer.launch({headless: false})  
+    const browser = await puppeteer.launch({ headless: false })  
     const page = await browser.newPage();
     await page.goto('https://pluto.tv/latam/live-tv/63eb9255c111bc0008fe6ec4');
     await page.setViewport({ width: 1366, height: 768 });
 
     let scraped = new Set()
 
-    async function load_content(){
-      const selector = 'div[role="rowheader"] > a'
+    async function load_content(selector){
       await page.waitForSelector(selector)
       let elements = await page.$$(selector);
       return elements
     }
+  
+    const sections = await load_content('div> ul > li > div > button')
+    const elementsSet = new Set()
 
+    for (const section of sections) 
+    {
+      await section.click();
+      const elements = await load_content('div[role="rowheader"] > a');
 
-    let j = 0
-    let visited_links = new Set()
-    for(let i = 0; i < 100; i++) {
-      
-      console.log(`${j}`);
-      let elements = await load_content()
-      if (elements.length == j){
-        console.log("Entro al IF de elemnts.lenth")
-        let elements = await load_content()
-        j = 0
-        await page.mouse.wheel({deltaY: 100});
-      }
-       if (elements[j] && !visited_links.has(elements[j])) {
+      for (const element of elements) {
+        try {
+          //Consigue el link
+          const href = `https://pluto.tv/latam${await page.evaluate(el => el.getAttribute('href'), element)}`
+          const name = "hi"//await page.evaluate(el => el.getAttribute('aria-label'), element)
+          const dict = {
+            'name': name,
+            'href': href
+          }
+          elementsSet.add(dict)
+
           
-           await elements[j].click();
-           await new Promise(r => setTimeout(r, 500)); // Espera 1 segundo
-           j++
-            const content = await page.evaluate(() => {
-                const titleElement = document.querySelector('#overlay-container > div > div > div > section > div > div.inner > h2');
-                const title = titleElement ? titleElement.title : 'No title found';
-                const url = document.URL
-                
-                return {
-                    'title': title,
-                    'url':url
-                };
-            });
-            visited_links.add(content.url)
-            console.log(visited_links)
-            scraped.add(content)
-           await page.keyboard.press('Escape');
-       }
-  }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+  
+      }
+    }
 
-    console.timeEnd()
-    console.log(scraped)
+    fs.writeFileSync('live_tv.json', JSON.stringify([...elementsSet], null, 2))
+
+    console.timeEnd(timer)
+    console.log("Channels Scraped: ", elementsSet.size)
 }
 scraper_starter()
 
