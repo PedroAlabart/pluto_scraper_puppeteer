744d93aeb643b8d67ae43040d409a367951af4c4
Pedro <pedroalabart@hotmail.com>
2024-08-20 02:47:22 -0300

    Fixed indentation

--- live_tv_scraper.js
+++ live_tv_scraper.js
@@ -6,45 +6,41 @@ const timer = "Live TV - Scraping Time"
 export async function live_tv_scraper_starter() {
   console.time(timer)
 
-    const browser = await puppeteer.launch({ headless: false })  
-    const page = await browser.newPage();
-    await page.goto('https://pluto.tv/latam/live-tv/63eb9255c111bc0008fe6ec4');
-    await page.setViewport({ width: 1366, height: 768 });
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage();
+  await page.goto('https://pluto.tv/latam/live-tv/63eb9255c111bc0008fe6ec4');
+  await page.setViewport({ width: 1366, height: 768 });
 
-    let scraped = new Set()
+  let scraped = new Set()
 
-    async function load_content(selector){
-      await page.waitForSelector(selector)
-      let elements = await page.$$(selector);
-      return elements
-    }
-  
-    const sections = await load_content('div> ul > li > div > button')
-    const elementsSet = new Set()
+  async function load_content(selector) {
+    await page.waitForSelector(selector)
+    let elements = await page.$$(selector);
+    return elements
+  }
 
-    for (const section of sections) 
-    {
-      await section.click();
-      const elements = await load_content('div[role="rowheader"] > a');
+  const sections = await load_content('div > ul > li > div > button')
+  const elementsSet = new Set()
 
-      for (const element of elements) {
-        try {
-          //Consigue el link
-          const href = `https://pluto.tv/latam${await page.evaluate(el => el.getAttribute('href'), element)}`
-          const name = await page.evaluate(el => el.children[1].getAttribute('aria-label'), element)
-          elementsSet.add(name)
+  for (const section of sections) {
+    await section.click();
+    const elements = await load_content('div[role="rowheader"] > a');
 
-          
-        } catch (error) {
-          console.error('Error:', error);
-        }
-  
+    for (const element of elements) {
+      try {
+        // Consigue el link
+        const href = `https://pluto.tv/latam${await page.evaluate(el => el.getAttribute('href'), element)}`
+        const name = await page.evaluate(el => el.children[1].getAttribute('aria-label'), element)
+        elementsSet.add(name)
+
+      } catch (error) {
+        console.error('Error:', error);
       }
     }
+  }
 
-    fs.writeFileSync('live_tv_scraped.json', JSON.stringify([...elementsSet], null, 2))
+  fs.writeFileSync('live_tv_scraped.json', JSON.stringify([...elementsSet], null, 2))
 
-    console.timeEnd(timer)
-    console.log("Channels Scraped: ", elementsSet.size)
+  console.timeEnd(timer)
+  console.log("Channels Scraped: ", elementsSet.size)
 }
-
--- on_demand_scraper.js
+++ on_demand_scraper.js
@@ -4,64 +4,60 @@ import { scrape_content } from './on_demand_util.js'
 
 const timer = "On Demand - Scraping Time"
 
-export async function on_demand_scraper_starter(){
-    console.time(timer)
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage()
-  
-    // Navigate the page to a URL
-    await page.goto('https://pluto.tv/latam/on-demand?lang=en')
-    await page.setViewport({ width: 1920, height: 1080 })
+export async function on_demand_scraper_starter() {
+  console.time(timer)
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
 
-    await page.locator('ul.categoryList > li a img').filter(a => a.classList == "").click()
+  // Navigate the page to a URL
+  await page.goto('https://pluto.tv/latam/on-demand?lang=en')
+  await page.setViewport({ width: 1920, height: 1080 })
 
-    let scraped = new Set()
+  await page.locator('ul.categoryList > li a img').filter(a => a.classList == "").click()
 
-    
-    const initialContent = await scrape_content(page)
-    if (initialContent) {
+  let scraped = new Set()
+
+  const initialContent = await scrape_content(page)
+  if (initialContent) {
+    scraped.add(initialContent)
+  }
+
+  // for (let i = 0; i < 200; i++)
+  while (true) {
+    const is_media = await page.evaluate(() => {
+      const activeElement = document.activeElement
+      const firstChild = activeElement.firstElementChild
+      return firstChild && firstChild.tagName === "IMG"
+    })
+
+    const is_end_of_records = await page.evaluate(() => {
+      const activeElement = document.activeElement.classList
+      return activeElement.contains('footer-link-terms_of_use-atc')
+    })
+
+    if (is_end_of_records) {
+      break
+    }
+
+    if (is_media) {
+      await page.keyboard.press('Enter')
+      const initialContent = await scrape_content(page)
+      if (initialContent) {
         scraped.add(initialContent)
+      }
     }
 
-     //for (let i = 0; i < 200; i++)
-      while(true)
-        {
-        const is_media = await page.evaluate(() => {
-            const activeElement = document.activeElement
-            
-            const firstChild = activeElement.firstElementChild
-            
-            return firstChild && firstChild.tagName === "IMG"
-        })
+    await page.keyboard.press('Escape')
+    await new Promise(r => setTimeout(r, 300))
+    await page.keyboard.press('Tab')
 
-        const is_end_of_records = await page.evaluate(() => {
-            const activeElement = document.activeElement.classList
-            
-            return activeElement.contains('footer-link-terms_of_use-atc')
-        })
-        if (is_end_of_records){
-            break
-        }
-        if (is_media) {
-            await page.keyboard.press('Enter')
-            const initialContent = await scrape_content(page)
-            if (initialContent) {
-                scraped.add(initialContent)
-            }
-        }
-        
-        await page.keyboard.press('Escape')
-        await new Promise(r => setTimeout(r, 300))
-        await page.keyboard.press('Tab')
+    // await new Promise(r => setTimeout(r, 400))
+  }
 
-        // await new Promise(r => setTimeout(r, 400))
-    }
-
-    fs.writeFileSync('on_demand_scraped.json', JSON.stringify([...scraped], null, 2))
-    console.timeEnd(timer)
-    console.log("Media Scraped: ", scraped.size)
-
+  fs.writeFileSync('on_demand_scraped.json', JSON.stringify([...scraped], null, 2))
+  console.timeEnd(timer)
+  console.log("Media Scraped: ", scraped.size)
 }
 
 // on_demand_scraper_starter()
